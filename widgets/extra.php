a95b03441ac525b7cad52da9c83d4d9cfc49ce05
JorensM <jorensmerenjanu@gmail.com>
2022-10-12 14:54:52 +0300

    stuff

--- widgets/extra.php
+++ widgets/extra.php
@@ -1,3 +1,14 @@
+<span class='mts-product-title'><?php echo $product->get_name()?></span>
+<div class='mts-info'>
+                    <?php render_info("calendar.svg", $match_date) ?>
+                    <span class='mts-info-spacer'></span>
+                    <?php render_info("clock.svg", $match_time) ?>
+                    <span class='mts-info-spacer'></span>
+                    <?php render_info("location.svg", $match_location) ?>
+                    <span class='mts-info-spacer'></span>
+                    <?php render_info("trophy.svg", $match_championship) ?>
+                    <span class='mts-info-spacer'></span>
+                </div>
 <div class='mts-product-info'>
                     <div class='mts-product-info-left'>
                         <div class='mts-tickets'>
@@ -55,3 +66,131 @@
                         </div>
                     </div>
                 </div>
+
+                <script>
+                    const error_element = document.getElementById("mts-error");
+
+                    const product_id = <?php echo $product->get_id() ?>
+
+                    let total_price = 0;
+
+                    const category_prices = [
+                        <?php echo $category_prices[0] ?>,
+                        <?php echo $category_prices[1] ?>,
+                        <?php echo $category_prices[2] ?>,
+                        <?php echo $category_prices[3] ?>,
+                    ];
+
+                    let tickets_qty = 1;
+                    const tickets_qty_middle_element = document.getElementById("mts-tickets-qty");
+                    const tickets_qty_subtract_element = document.getElementById("mts-tickets-qty-subtract");
+                    render_tickets_subtract();
+
+                    let selected_category = 1;
+                    select_category(1);
+                    
+
+                    render_total_price();
+
+                    function subtract_ticket(){
+                        console.log("123");
+                        if(tickets_qty > 1){
+                            tickets_qty--;
+                        }
+                        render_tickets_select();
+                        console.log("aaa");
+                        render_total_price();
+                    }
+
+                    function add_ticket(){
+                        
+                        tickets_qty++;
+                        render_tickets_select();
+                        render_total_price();
+                    }
+
+                    function render_tickets_qty(){
+                        tickets_qty_middle_element.innerHTML = tickets_qty + (tickets_qty > 1 ? " tickets" : " ticket");
+                    }
+
+                    function render_tickets_subtract(){
+                        if(tickets_qty <= 1){
+                            tickets_qty_subtract_element.classList.add("mts-tickets-qty-subtract-disabled");
+                        }else{
+                            tickets_qty_subtract_element.classList.remove("mts-tickets-qty-subtract-disabled");
+                        }
+                    }
+
+                    function render_tickets_select(){
+                        render_tickets_qty();
+                        render_tickets_subtract();
+                    }
+
+                    function select_category(category_number){
+                        selected_category = category_number;
+
+                        const category_button_element = document.getElementById("mts-category-button-" + category_number);
+
+                        deselect_all_categories();
+                        
+                        category_button_element.classList.add("mts-category-button-selected");
+                        category_button_element.classList.add("mts-cat" + category_number + "-button-selected");
+
+                        render_total_price();
+                    }
+
+                    function deselect_all_categories(){
+                        for(let i = 1; i < 4; i++){
+                            const category_button_element = document.getElementById("mts-category-button-" + i);
+
+                            if(category_button_element !== null){
+                                category_button_element.classList.remove("mts-category-button-selected");
+                                category_button_element.classList.remove("mts-cat" + i + "-button-selected");
+                            }
+                        }
+                    }
+
+                    function render_total_price(){
+                        console.log(category_prices);
+                        console.log(tickets_qty);
+                        total_price = category_prices[selected_category - 1] * tickets_qty;
+                        console.log(total_price);
+                        
+                        const total_price_element = document.getElementById("mts-total-price");
+
+                        total_price_element.innerHTML = total_price;
+                    }
+
+                    function goto_checkout(){
+                        error_element.innerHTML = "";
+                        const add_to_cart_url = "<?php echo plugins_url("add_to_cart.php", __FILE__) ?>";
+
+                        const data = {
+                            product_id: product_id,
+                            category: selected_category,
+                            qty: tickets_qty
+                        }
+
+                        const request = new Request(add_to_cart_url, {
+                            method: "POST",
+                            body: JSON.stringify(data)
+                        })
+
+                        fetch(request)
+                        .then(response => response.text())
+                        .then(data => {
+                            console.log("success");
+                            console.log(data);
+                            if(data === '"success"'){
+                                console.log("aaa");
+                                window.location.href = "<?php echo wc_get_checkout_url(); ?>";
+                            }
+                        })
+                        .catch(err => {
+                            console.log("error");
+                            console.log(err);
+                            error_element.innerHTML = "Error: <br>" + err;
+                        });
+                    }
+
+                </script>
--- widgets/product-widget.php
+++ widgets/product-widget.php
@@ -167,182 +167,36 @@ class Elementor_Product_Widget extends \Elementor\Widget_Base {
 
 	protected function render() {
         global $product;
-        // global $match_date;
-        // global $match_time;
-        // global $match_location;
-        // global $match_championship;
-        //global $team_1_img_url;
-        //global $team_2_img_url;
-        global $categories;
-        //global $stadium_img_url;
 
-        $match_date = date("l, j F", strtotime($product->get_meta("match-date")));
-        $match_time = $product->get_meta("match-time");
-        $match_location = $product->get_meta("match-location");
-        $match_championship = $product->get_meta("championship-name");
+        // $match_date = date("l, j F", strtotime($product->get_meta("match-date")));
+        // $match_time = $product->get_meta("match-time");
+        // $match_location = $product->get_meta("match-location");
+        // $match_championship = $product->get_meta("championship-name");
 
-        $team_1_img_url = $product->get_meta("1st-team-image");
-        $team_2_img_url = $product->get_meta("2nd-team-image");
-        $stadium_img_url = wp_get_attachment_image_src( get_post_thumbnail_id( $product->get_id() ), 'single-post-thumbnail' )[0];
+        // $team_1_img_url = $product->get_meta("1st-team-image");
+        // $team_2_img_url = $product->get_meta("2nd-team-image");
+        // $stadium_img_url = wp_get_attachment_image_src( get_post_thumbnail_id( $product->get_id() ), 'single-post-thumbnail' )[0];
 
-        $categories = [
-            get_variation_by_name($product, "Category 1"),
-            get_variation_by_name($product, "Category 2"),
-            get_variation_by_name($product, "Category 3"),
-            get_variation_by_name($product, "Category 4")
-        ];
+        // $categories = [
+        //     get_variation_by_name($product, "Category 1"),
+        //     get_variation_by_name($product, "Category 2"),
+        //     get_variation_by_name($product, "Category 3"),
+        //     get_variation_by_name($product, "Category 4")
+        // ];
 
-        foreach($categories as $key => $category){
-            if($category !== null){
-                $category_prices[$key] = $category->get_regular_price();
-            }else{
-                $category_prices[$key] = "null";
-            }
+        // foreach($categories as $key => $category){
+        //     if($category !== null){
+        //         $category_prices[$key] = $category->get_regular_price();
+        //     }else{
+        //         $category_prices[$key] = "null";
+        //     }
             
-        }
-            ?>
+        // }
+            //echo $product->get_id();
 
-                <span class='mts-product-title'><?php echo $product->get_name()?></span>
-                <div class='mts-info'>
-                    <?php render_info("calendar.svg", $match_date) ?>
-                    <span class='mts-info-spacer'></span>
-                    <?php render_info("clock.svg", $match_time) ?>
-                    <span class='mts-info-spacer'></span>
-                    <?php render_info("location.svg", $match_location) ?>
-                    <span class='mts-info-spacer'></span>
-                    <?php render_info("trophy.svg", $match_championship) ?>
-                    <span class='mts-info-spacer'></span>
-                </div>
-                
-                
-                <script>
-                    const error_element = document.getElementById("mts-error");
-
-                    const product_id = <?php echo $product->get_id() ?>
-
-                    let total_price = 0;
-
-                    const category_prices = [
-                        <?php echo $category_prices[0] ?>,
-                        <?php echo $category_prices[1] ?>,
-                        <?php echo $category_prices[2] ?>,
-                        <?php echo $category_prices[3] ?>,
-                    ];
-
-                    let tickets_qty = 1;
-                    const tickets_qty_middle_element = document.getElementById("mts-tickets-qty");
-                    const tickets_qty_subtract_element = document.getElementById("mts-tickets-qty-subtract");
-                    render_tickets_subtract();
-
-                    let selected_category = 1;
-                    select_category(1);
-                    
-
-                    render_total_price();
-
-                    function subtract_ticket(){
-                        console.log("123");
-                        if(tickets_qty > 1){
-                            tickets_qty--;
-                        }
-                        render_tickets_select();
-                        console.log("aaa");
-                        render_total_price();
-                    }
-
-                    function add_ticket(){
-                        
-                        tickets_qty++;
-                        render_tickets_select();
-                        render_total_price();
-                    }
-
-                    function render_tickets_qty(){
-                        tickets_qty_middle_element.innerHTML = tickets_qty + (tickets_qty > 1 ? " tickets" : " ticket");
-                    }
-
-                    function render_tickets_subtract(){
-                        if(tickets_qty <= 1){
-                            tickets_qty_subtract_element.classList.add("mts-tickets-qty-subtract-disabled");
-                        }else{
-                            tickets_qty_subtract_element.classList.remove("mts-tickets-qty-subtract-disabled");
-                        }
-                    }
-
-                    function render_tickets_select(){
-                        render_tickets_qty();
-                        render_tickets_subtract();
-                    }
-
-                    function select_category(category_number){
-                        selected_category = category_number;
-
-                        const category_button_element = document.getElementById("mts-category-button-" + category_number);
-
-                        deselect_all_categories();
-                        
-                        category_button_element.classList.add("mts-category-button-selected");
-                        category_button_element.classList.add("mts-cat" + category_number + "-button-selected");
-
-                        render_total_price();
-                    }
-
-                    function deselect_all_categories(){
-                        for(let i = 1; i < 4; i++){
-                            const category_button_element = document.getElementById("mts-category-button-" + i);
-
-                            if(category_button_element !== null){
-                                category_button_element.classList.remove("mts-category-button-selected");
-                                category_button_element.classList.remove("mts-cat" + i + "-button-selected");
-                            }
-                        }
-                    }
-
-                    function render_total_price(){
-                        console.log(category_prices);
-                        console.log(tickets_qty);
-                        total_price = category_prices[selected_category - 1] * tickets_qty;
-                        console.log(total_price);
-                        
-                        const total_price_element = document.getElementById("mts-total-price");
-
-                        total_price_element.innerHTML = total_price;
-                    }
-
-                    function goto_checkout(){
-                        error_element.innerHTML = "";
-                        const add_to_cart_url = "<?php echo plugins_url("add_to_cart.php", __FILE__) ?>";
-
-                        const data = {
-                            product_id: product_id,
-                            category: selected_category,
-                            qty: tickets_qty
-                        }
-
-                        const request = new Request(add_to_cart_url, {
-                            method: "POST",
-                            body: JSON.stringify(data)
-                        })
-
-                        fetch(request)
-                        .then(response => response.text())
-                        .then(data => {
-                            console.log("success");
-                            console.log(data);
-                            if(data === '"success"'){
-                                console.log("aaa");
-                                window.location.href = "<?php echo wc_get_checkout_url(); ?>";
-                            }
-                        })
-                        .catch(err => {
-                            console.log("error");
-                            console.log(err);
-                            error_element.innerHTML = "Error: <br>" + err;
-                        });
-                    }
-
-                </script>
-
+            //$product_id = $product->get_id();
+            ?>  
+                <span>Hello</span>
             <?php
 	}
 }
